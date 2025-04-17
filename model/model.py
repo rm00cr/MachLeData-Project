7c8c46ede1c72d6bcede1e11faa1d9f9392edab9
glenn <glenn@DISCHEADWin>
2025-04-17 11:21:03 +0200

    added dockerfile

--- model/model.py
+++ model/model.py
@@ -1,19 +1,27 @@
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import RandomForestRegressor
+import mlflow
+import mlflow.sklearn
 
 
 def rf_regressor(X_train, y_train):
-    rf = RandomForestRegressor(n_estimators=300, random_state=42, max_depth=12)
-    rf.fit(X_train, y_train.ravel())
+    mlflow.set_experiment("RandomForest_regression")
+    mlflow.sklearn.autolog()
+    with mlflow.start_run():
+        rf = RandomForestRegressor(n_estimators=300, random_state=42, max_depth=12)
+        rf.fit(X_train, y_train.ravel())
+        mlflow.sklearn.save_model(rf, path="./saved_models/rf_model")
     return rf
 
 def boosting_regressor(X_train, y_train):
-    gb = GradientBoostingRegressor(n_estimators=600, random_state=42, max_depth=12, learning_rate=0.9)
-    gb.fit(X_train, y_train.ravel())
+    mlflow.set_experiment("gradient_boosting_regression")
+    mlflow.sklearn.autolog()
+    with mlflow.start_run():
+        gb = GradientBoostingRegressor(n_estimators=600, random_state=42, max_depth=12, learning_rate=0.9)
+        gb.fit(X_train, y_train.ravel())
+        mlflow.sklearn.save_model(gb, path="./saved_models/gb_model")
     return gb
 
 def predict_salary(model, X_test):
-
     y_pred = model.predict(X_test.toarray())
-
     return y_pred
--- model/preprocessing.py
+++ model/preprocessing.py
@@ -1,9 +1,10 @@
 import kagglehub
 import pandas as pd
 from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.model_selection import train_test_split
+import joblib
+
 
 def download_dataset():
     """
@@ -12,34 +13,36 @@ def download_dataset():
     dataset_path = kagglehub.dataset_download("samithsachidanandan/the-global-ai-ml-data-science-salary-for-2025")
     return dataset_path
 
+
 def load_data(dataset_path):
     data = pd.read_csv(dataset_path + "/salaries.csv")
     return data
 
-def preprocess_data(data):
-    
+
+def preprocess_data(data, debug=False):
+
     categorical_features = ["experience_level", "employment_type", "job_title", "company_size", "company_location", "employee_residence", "salary_currency"]
 
+    categorical_transformer = ColumnTransformer([("onehot", OneHotEncoder(handle_unknown="ignore"), categorical_features)], remainder="passthrough")
+
+    if debug:
+        categorical_transformer = joblib.load("./categorical_transformer.pkl")
+        return categorical_transformer.transform(data)
+    
     data = data.query('job_title == "Data Scientist"')
-
     x_train, x_test, y_train, y_test = train_test_split(data.drop(columns=["salary", "salary_in_usd", "work_year"]), data["salary"],  test_size=0.15, random_state=42, shuffle=True)
-
-   
-    categorical_transformer =  ColumnTransformer([("onehot", OneHotEncoder(handle_unknown="ignore"), categorical_features)], remainder="passthrough")
     preprocessed_x_train = categorical_transformer.fit_transform(x_train)
+    joblib.dump(categorical_transformer, "./categorical_transformer.pkl")
     preprocessed_x_test = categorical_transformer.transform(x_test)
 
     print(preprocessed_x_train.shape)
     print(preprocessed_x_train.shape)
-  
-  
+
     scaler = StandardScaler()   
     scaler.fit(y_train.values.reshape(-1, 1))
-    #y_train = scaler.transform(y_train.values.reshape(-1, 1))
-    #y_test = scaler.transform(y_test.values.reshape(-1, 1))
+    # y_train = scaler.transform(y_train.values.reshape(-1, 1))
+    # y_test = scaler.transform(y_test.values.reshape(-1, 1))
     print(y_train.shape)
     print(y_test.shape)
 
     return preprocessed_x_train, preprocessed_x_test, y_train.values.reshape(-1, 1), y_test.values.reshape(-1, 1)
-
-
