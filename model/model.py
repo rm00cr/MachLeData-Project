2d83b623dee48d46cf4ea59ba7c64d99b70f0808
glenn <glace@OnFire>
2025-05-07 07:58:49 +0200

    model

--- model/model.py
+++ model/model.py
@@ -5,13 +5,17 @@ import mlflow
 import mlflow.sklearn
 from mlflow.models import infer_signature
 
+
 def train_and_evaluate(model, X_train, y_train, X_test, y_test):
     if model == "rf":
-        model = RandomForestRegressor(n_estimators=300, random_state=42, max_depth=12)
+        model = RandomForestRegressor(
+            n_estimators=300, random_state=42, max_depth=12)
     elif model == "gb":
-        model = GradientBoostingRegressor(n_estimators=600, random_state=42, max_depth=12, learning_rate=0.9)
+        model = GradientBoostingRegressor(
+            n_estimators=600, random_state=42, max_depth=12, learning_rate=0.9)
     else:
-        raise ValueError("Model not supported. Choose 'rf' for Random Forest or 'gb' for Gradient Boosting.")
+        raise ValueError(
+            "Model not supported. Choose 'rf' for Random Forest or 'gb' for Gradient Boosting.")
     with mlflow.start_run():
         mlflow.sklearn.autolog()
         model.fit(X_train, y_train.ravel())
@@ -22,7 +26,7 @@ def train_and_evaluate(model, X_train, y_train, X_test, y_test):
             artifact_path="model_registry",
             signature=signature,
             registered_model_name="sk-learn-random-forest-reg-model")
-        #mlflow.sklearn.save_model(model, path="./saved_models/rf_model")
+        mlflow.sklearn.save_model(model, path="./saved_models/rf_model")
         y_pred = model.predict(X_test.toarray())
         mlflow.log_metric("r2_score", r2_score(y_test, y_pred))
         mlflow.log_metric("mse", mean_squared_error(y_test, y_pred))
